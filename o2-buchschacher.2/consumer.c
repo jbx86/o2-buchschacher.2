2f690f5a54c12a743d67f3ff0d25eba3aae2f0e3
Joseph Buchschacher <jbx86@mail.umsl.edu>
2018-02-20 17:02:41 -0600

    added code to master that run consumer and producer from litteral strings

--- o2-buchschacher.2/consumer.c
+++ o2-buchschacher.2/consumer.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	fprintf(stderr, "\nconsumer running");
 	int i;
 	for (i = 0; i < argc; i++)
 		fprintf(stderr, "%s ", argv[i]);
--- o2-buchschacher.2/master.c
+++ o2-buchschacher.2/master.c
@@ -4,60 +4,76 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int makeargv(const char *s, const char *delimiters, char ***argvp);
+
 int main(int argc, char *argv[]) {
 	pid_t pid;
-	int i;
+	int i = 0;
 	int n = 10;
 	int x = 7;
+
+	char delim[] = " \t";
+	char **myargv;
+	int numtokens;
+	char *strProd = "./producer 1 2";
+	char *strCons = "./consumer 3 4";
+
 	
-	//char *buffer;
-
-	char *cmd = "./consumer";
-	char *buff[] = {"./consumer", "4", "5", NULL};
-
-	fprintf(stderr, "%s\n", buff[2]);
-	sprintf(buff[2], "%d", n);
-	fprintf(stderr, "%s\n", buff[2]);
-
+	// Determine n value from commandline
 	if (argc > 2) {
 		fprintf(stderr, "Usage: %s n", argv[0]);
 	}
 
-	if (argc == 1)
-		n = 10;
+	if (argc == 1) {
+		n = 10;		// Default to 10
+	}
 	else
 	{
 		n = atoi(argv[1]);
 		if (n > 20)
-			n = 20;
+			n = 20;		// Restrict to 20 processes
 		else if (n < 1) {
 			fprintf(stderr, "Invalid input '%s'\n", argv[1]);
 			return 1;
 		}
 	}
-	
-	for (i = 0; i <= n; i++) {
+
+
+	// Fork n processes
+	for (i = 1; i < n; i++) {
 		fprintf(stderr, "%d", i);
-		pid = fork();
+		pid = fork(); //Fork proccess
 		if (pid == 0) {
-			// char *cmd = "./consumer";
-			//char *buff[] = {"./consumer", "4", "5", NULL};
-			execvp(cmd, buff);		
+			//Execute children as producer or consumer(s)
+			if (i == 1) {
+				// Construct producer arguments
+				if (makeargv(strProd, delim, &myargv) == -1)
+					fprintf(stderr, "Failed to construct argument array\n");
+				else
+					execvp(myargv[0], &myargv[0]);
+			}
+			else {
+				// Construct consumer arguments
+				if (makeargv(strCons, delim, &myargv) == -1)
+					fprintf(stderr, "Failed to construct argument array\n");
+				else
+					execvp(myargv[0], &myargv[0]);
+			}
 		}
-		wait();		
-/*
-		pid = fork();
-		if (i == 0) {
-			cmd = "producer";
-			buff[0] = "producer";
-		}
-		else {
-			cmd = "consumer";
-			buff[0] = "consumer";
-		}
-		execvp(cmd, buff);
-		*/
+		wait();
 	}
+
+
+/*
+	//Test output of variabls
+	fprintf(stderr, "process %d executed as 'master'\n", i++);
+	fprintf(stderr, "process %d executed as 'producer'\n", i++);
+	while (i < n) {
+		fprintf(stderr, "process %d executed as 'consumer'\n", i++);	
+	}
+*/
+
+	
 
 }
 
